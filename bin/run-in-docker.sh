b1fd08e3f0d80c5fac68ec24bd42a3d532f9b0d2
Dinis Cruz <dinis.cruz@owasp.org>
2023-01-26 13:26:14 +0000

    Trying version 0.107.0 of Hugo

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -1,5 +1,6 @@
 #HUGO_VERSION=0.91.2
 HUGO_VERSION=0.93.2
+HUGO_VERSION=0.107.0
 
 # need to use {$version}ext-alpine because it has the git executable
 docker run -v $PWD:/src -p 1313:1313 --rm -it klakegg/hugo:${HUGO_VERSION}-ext-alpine serve
